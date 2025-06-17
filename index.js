b3d7b642dac7e01a4d45ae99f5a9ee459b0b853b
NgothTsika <ngothtsikavivien@gmail.com>
2025-06-17 17:51:01 +0000

    Add shortid and nanoid libraries for unique ID generation

--- index.js
+++ index.js
@@ -56,26 +56,80 @@
 
 //TEST 2
 
+// const express = require("express");
+// const cors = require("cors");
+// const app = express();
+
+// app.use(cors());
+// app.use(express.static("public"));
+
+// app.get("/", (req, res) => {
+//   res.send("Header Parser Microservice is running!");
+// });
+
+// app.get("/api/whoami", (req, res) => {
+//   res.json({
+//     ipaddress: req.ip,
+//     language: req.headers["accept-language"],
+//     software: req.headers["user-agent"],
+//   });
+// });
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
+
+//TEST 3
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
+const dns = require("dns");
+const urlParser = require("url");
+const shortid = require("shortid");
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(express.static("public"));
 
+const urls = {}; // In-memory storage
+
 app.get("/", (req, res) => {
-  res.send("Header Parser Microservice is running!");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/api/whoami", (req, res) => {
-  res.json({
-    ipaddress: req.ip,
-    language: req.headers["accept-language"],
-    software: req.headers["user-agent"],
+// POST endpoint to shorten a URL
+app.post("/api/shorturl", (req, res) => {
+  const original_url = req.body.url;
+  const hostname = urlParser.parse(original_url).hostname;
+
+  dns.lookup(hostname, (err) => {
+    if (err) {
+      return res.json({ error: "invalid url" });
+    } else {
+      const short_url = shortid.generate().slice(0, 6);
+      urls[short_url] = original_url;
+      return res.json({ original_url, short_url });
+    }
   });
+});
+
+// GET endpoint to redirect to original URL
+app.get("/api/shorturl/:short_url", (req, res) => {
+  const short_url = req.params.short_url;
+  const original_url = urls[short_url];
+
+  if (original_url) {
+    res.redirect(original_url);
+  } else {
+    res.status(404).json({ error: "No short URL found for given input" });
+  }
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
